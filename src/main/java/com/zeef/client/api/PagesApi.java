1be1139c666795bea4b1cad23f37034c9a95b91e
Jan Beernink <jan.beernink@gmail.com>
2015-04-22 20:41:02 +0200

    Added search functionality to the library

--- src/main/java/com/zeef/client/api/PagesApi.java
+++ src/main/java/com/zeef/client/api/PagesApi.java
@@ -9,9 +9,9 @@ package com.zeef.client.api;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,6 @@ import javax.ws.rs.core.Form;
 import javax.ws.rs.core.GenericType;
 
 import com.zeef.client.ApiInvoker;
-import com.zeef.client.model.Page;
 import com.zeef.client.model.Page.PageTypeEnum;
 import com.zeef.client.model.PagesOverview;
 
@@ -148,5 +147,57 @@ public class PagesApi {
 		}, "/pages/mine", "GET", queryParams, pathParams, postBody, headerParams, contentType);
 
 	}
+
+  /**
+   * Search for pages
+   *
+   * @param query
+   * @param maxResults
+   * @param firstResult
+   * @param preferredLanguageCode
+   * @return PagesOverview
+   */
+  public PagesOverview search(String query, Integer maxResults, Integer firstResult, String preferredLanguageCode) {
+    Object postBody = null;
+
+
+    Map<String, String> pathParams = new HashMap<>();
+    Map<String, String> queryParams = new HashMap<>();
+    Map<String, String> headerParams = new HashMap<>();
+
+    if (query != null) {
+      queryParams.put("query", Objects.toString(query));
+    }
+
+    if (maxResults != null) {
+      queryParams.put("maxResults", Objects.toString(maxResults));
+    }
+
+    if (firstResult != null) {
+      queryParams.put("firstResult", Objects.toString(firstResult));
+    }
+
+    if (preferredLanguageCode != null) {
+      queryParams.put("preferredLanguageCode", Objects.toString(preferredLanguageCode));
+    }
+
+
+
+
+    String[] contentTypes = {
+      "application/x-www-form-urlencoded",
+    };
+
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+
+    if(contentType.equals(APPLICATION_FORM_URLENCODED)) {
+      Form form = new Form();
+
+      postBody = form;
+    }
+
+    return apiInvoker.invokeAPI(new GenericType<PagesOverview>(){}, "/pages/search", "GET", queryParams, pathParams, postBody, headerParams, contentType);
+
+  }
 
 }
